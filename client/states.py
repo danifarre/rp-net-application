8eea586b7212354def053c75d6ea1da7e876c1a7
Daniel Farre <danifarre97@gmail.com>
2020-02-23 19:23:32 +0100

    Implementacion de los estados

---
+++ client/states.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+# Clase que representa els estats possibles d'un client
+
+actual_state = ""
+
+class States:
+
+    def __init__(self, state = "NOT_REGISTERED"):
+        self.actual_state = state
+
+    def get_actual_state(self):
+        return self.actual_state
+
+    def to_NOT_REGISTERED(self):
+        self.actual_state = "NOT_REGISTERED"
+
+    def to_WAIT_ACK_REG(self):
+        self.actual_state = "WAIT_ACK_REG"
+
+    def to_WAIT_INFO(self):
+        self.actual_state = "WAIT_INFO"
+
+    def to_WAIT_ACK_INFO(self): 
+        self.actual_state = "WAIT_ACK_INFO"
+
+    def to_REGISTERED(self):
+        self.actual_state = "REGISTERED"
+
+    def to_SEND_ALIVE(self):
+        self.actual_state = "SEND_ALIVE"
+
+    def show(self):
+        print self.actual_state
