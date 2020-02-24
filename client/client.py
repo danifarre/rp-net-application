2c7725f7acb98cb7465a58493b9a921b69032c62
Daniel Farre <danifarre97@gmail.com>
2020-02-24 18:54:22 +0100

    Carpeta practica1 añadida

--- client/client.py
+++ client/client.py
@@ -15,7 +15,7 @@ class Client:
 
     def registry(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto('' + configuration['id'] + ' 00000000 ' + '', (self.configuration['server'], int(self.configuration['server-udp'])))
+        sock.sendto('(0x00) (' + configuration['id'] + ') ("00000000")' + ' ("")', (self.configuration['server'], int(self.configuration['server-udp'])))
 
 def read_configuration(file_name):
     configuration = {}
